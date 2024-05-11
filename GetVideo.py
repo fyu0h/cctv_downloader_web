9fbb916951226de9a1220ef07176f88520cb9971
fyu0h <33602841+fyu0h@users.noreply.github.com>
2024-05-11 15:37:35 +0800

    Update GetVideo.py
    新增tv.cctv.com/*下载支持

--- GetVideo.py
+++ GetVideo.py
@@ -1,62 +1,92 @@
-# GetVideo.py
 # -*- coding:utf-8 -*-
 import requests
 import subprocess
 import re
 import os
-import ffmpeg
-def download_video(url):
-    # 从用户获取原始链接
-    input_url = url
-    # 从输入的URL中提取GUID
-    guid_match = re.search(r'guid=([0-9a-fA-F\-]+)', input_url)
-    if not guid_match:
-        print("未找到有效的GUID，请确认链接是否正确")
-        return {"error": "未找到有效的GUID，请确认链接是否正确"}
+from dotenv import load_dotenv
 
-    guid = guid_match.group(1)
-    # 构造获取视频信息的API URL
-    api_url = f'https://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={guid}'
+#加载环境变量
+load_dotenv()
 
-    # 从API URL获取JSON数据
-    response = requests.get(api_url)
-    if response.status_code != 200:
-        print("无法获取视频信息。")
-        return {"error": "无法获取视频信息。"}
+# 从环境变量获取值
+DOWNLOAD_BASE_URL = os.getenv('DOWNLOAD_URL')
+PORT = os.getenv('PORT')
 
-    video_info = response.json()
-    if 'hls_url' not in video_info:
-        return {"error": "无有效的视频下载链接。"}
-
-    # 从JSON数据中获取视频标题和m3u8下载链接
-    title = video_info['title']
-    hls_url = video_info['hls_url']
-    orgin_url = url
-
-    # 视频标题用于命名输出文件
-    output_file = f"{title}.mp4"
-
-    # 下载目录
-    download_dir = "download"
-
-    # 确保下载目录存在，如果不存在则创建
-    if not os.path.exists(download_dir):
-        os.makedirs(download_dir)
-
-    # 如果输出文件已存在于下载目录中，则删除它
-    output_path = os.path.join(download_dir, output_file)
+# 从用户链接导出GUID
+def extract_guid(url): 
+    pattern = r"guid=([a-zA-Z0-9]+)" 
+    match = re.search(pattern, url) 
+    if match: 
+        print(match.group(1))
+        return match.group(1) 
+    return None 
+ 
+# 从用户链接导出VID
+def extract_vid(url): 
+    pattern = r"VID([a-zA-Z0-9]+)" 
+    match = re.search(pattern, url) 
+    if match: 
+        print(match.group(0))
+        return match.group(0) 
+        print ("match")
+    return None 
+ 
+# 下载合并m3u8并转码为mp4
+def download_and_merge(hls_url, title, input_url): 
+    output_dir = f'download'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_path = os.path.join(output_dir, f"{title}.mp4")
     if os.path.exists(output_path):
         os.remove(output_path)
+    subprocess.run(['ffmpeg', '-i', hls_url, '-c', 'copy', '-bsf:a', 'aac_adtstoasc', output_path]) 
+    download_link =  f"https://{DOWNLOAD_BASE_URL}/{output_dir}/{title}.mp4"
+    print(f"下载链接：https://{DOWNLOAD_BASE_URL}/{output_dir}/{title}.mp4")
+     # 返回视频标题、源网站和文件下载链接
+    return {"title": title, "url": input_url, "file": download_link}
 
-    # 使用ffmpeg下载并转换视频
-    command = f'ffmpeg -i "{hls_url}" -c copy -bsf:a aac_adtstoasc "{output_path}"'
-
-    # 执行命令
-    subprocess.run(command, shell=True)
-    # 输出下载链接
-    download_link = f"https://cntv.iinin.me:6688/download/{title}.mp4"
-    print(f"下载链接：{download_link}")
-
-    print(f"视频已保存为 {output_file}")
-    # 返回视频标题、源网站和文件下载链接
-    return {"title": video_info['title'], "url": input_url, "hls_url": video_info['hls_url'], "file": download_link}
+def fetch_api(url):
+    if "VID" in url:
+        vid = extract_vid(url)
+        if vid:
+            api_url = f"https://api.cntv.cn/Article/newContentInfo?serviceId=tvcctv&id={vid}&cb=abcde{vid}"
+            response = requests.get(api_url)
+            if response.status_code == 200:
+                start_index = response.text.find('"guid":') + len('"guid":')
+                end_index = response.text.find(',', start_index)
+                if start_index != -1 and end_index != -1:
+                    guid = response.text[start_index:end_index].strip('"')
+                    if guid:
+                        api_url = f"https://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={guid}"
+                        json_data = requests.get(api_url).json()
+                        if json_data:
+                            hls_url = json_data.get("hls_url")
+                            title = json_data.get("title")
+                            if hls_url and title:
+                                return download_and_merge(hls_url, title, url)
+                            else:
+                                print("Failed to extract HLS URL or title from JSON data.")
+                        else:
+                            print("Failed to retrieve JSON data from API.")
+                    else:
+                        print("GUID not found in response data.")
+                else:
+                    print("Failed to retrieve data from API. Status code:", response.status_code)
+            else:
+                print("Failed to extract VID from URL.")
+    else:
+        guid = extract_guid(url)
+        if guid:
+            api_url = f"https://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={guid}"
+            json_data = requests.get(api_url).json()
+            if json_data:
+                hls_url = json_data.get("hls_url")
+                title = json_data.get("title")
+                if hls_url and title:
+                    return download_and_merge(hls_url, title, url)
+                else:
+                    print("Failed to extract HLS URL or title from JSON data.")
+            else:
+                print("Failed to retrieve JSON data from API.")
+        else:
+            print("Failed to extract GUID from URL.")
